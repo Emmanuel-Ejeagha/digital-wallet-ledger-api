e889b76f917fb14d84bdb901d3f9dba29a72198b
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-02-14 21:24:35 +0100

    feat(domain): add base Entity class with ID-based equality

---
+++ src/DigitalWallet.Domain/Common/Entity.cs
@@ -0,0 +1,40 @@
+
+namespace DigitalWallet.Domain.Common;
+
+/// <summary>
+/// Base class for all domain entities. Equality is based on the entity's ID
+/// </summary>
+public abstract class Entity
+{
+    public Guid Id { get; private set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity other)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        if (GetType() != other.GetType())
+            return false;
+
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public static bool operator ==(Entity? left, Entity? right) => Equals(left, right);
+    public static bool operator !=(Entity? left, Entity? right) => !Equals(left, right);
+    
+}
