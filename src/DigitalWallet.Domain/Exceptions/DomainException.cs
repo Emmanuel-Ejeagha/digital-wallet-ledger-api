d489dac3913bef25d6e26a47a27b27fd1c85ded7
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-02-16 07:16:12 +0100

    feat(domain): add base domain exception type

---
+++ src/DigitalWallet.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DigitalWallet.Domain.Exceptions;
+/// <summary>
+/// Thrown when a domain invariant is violated.
+/// </summary>
+public class DomainException : Exception
+{
+    public DomainException() { }
+
+    public DomainException(string message) : base(message) { }
+
+    public DomainException(string message, Exception innerException) : base(message, innerException) {}
+}
