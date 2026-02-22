b7f6796047c8990031243b6cfe5aa93674a76829
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-02-22 20:35:42 +0100

    feat(infrastructure): add DependencyInjection for DbContext, repositories, and core services

---
+++ src/DigitalWallet.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,39 @@
+using DigitalWallet.Application.Common.Interfaces;
+using DigitalWallet.Infrastructure.Persistence;
+using DigitalWallet.Infrastructure.Repositories;
+using DigitalWallet.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DigitalWallet.Infrastructure;
+/// <summary>
+/// Extension method for registering infrastructure services
+/// </summary>
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // PostgreSQL  DbContext
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        // Repositories
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IAccountRepository, AccountRepository>();
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
+        services.AddScoped<IEmailService, EmailService>();
+        services.Configure<SmtpSettings>(configuration.GetSection("SmtpSettings"));
+
+        // Unit of Work
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        // Services]
+        services.AddScoped<IDateTime, DateTimeService>();
+        services.AddScoped<IIdempotencyService, IdempotencyService>();
+
+        return services;
+    }
+}
