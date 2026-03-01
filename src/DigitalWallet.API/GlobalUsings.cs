529856fa97c7b66a55687de7ec5f0bf0abf5856e
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-01 22:06:30 +0100

    chore(api): add health checks package

--- src/DigitalWallet.API/GlobalUsings.cs
+++ src/DigitalWallet.API/GlobalUsings.cs
@@ -23,3 +23,6 @@ global using DigitalWallet.Application.Common.Interfaces;
 global using DigitalWallet.Application.Features.Accounts.Commands;
 global using DigitalWallet.Application.Features.Transaction.Commands;
 global using DigitalWallet.Application.DTOs;
+global using DigitalWallet.API.Controllers.v1;
+global using DigitalWallet.API.Middleware;
+global using DigitalWallet.API.Services;
